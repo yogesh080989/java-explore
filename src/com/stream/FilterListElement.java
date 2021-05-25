f17a9e3fb6f8e945e068f8b11d7d626d388ec140
Jiten <yogeshahuja60@gmail.com>
2021-05-25 20:58:07 +0530

    TODO:Filter element

---
+++ src/com/stream/FilterListElement.java
@@ -0,0 +1,22 @@
+package com.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FilterListElement {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList();// initializing list
+		// adding element into list
+		for (int i = 1; i <= 20; i++) {
+			list.add(i);
+		}
+
+		Set<Integer> filterList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toSet());
+		System.out.println(filterList);	
+	}
+
+}
